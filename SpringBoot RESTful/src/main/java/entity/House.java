aad0deb4570f2c244e4ada86cbe8e459946bab0a
Neo1fight1 <17861080078@163.com>
2020-04-16 22:42:41 +0800

    Update House

--- SpringBoot RESTful/src/main/java/entity/House.java
+++ SpringBoot RESTful/src/main/java/entity/House.java
@@ -16,36 +16,65 @@ public class House {
         this.locationX = locationX;
         this.locationY = locationY;
     }
-
-    public House() {
-
+    public House(String houseID,String sellerID,double size, double price, double locationX,double locationY) {
+        this.houseID=houseID;
+        this.sellerID=sellerID;
+        this.size = size;
+        this.price = price;
+        this.locationX = locationX;
+        this.locationY = locationY;
     }
+    public House(){
 
-    public String getSellerID() {
-        return sellerID;
-    }
+    };
 
-    public double getSize() {
-        return size;
-    }
-
-    public double getPrice() {
-        return price;
+    public void setHouseID(String houseID) {
+        this.houseID = houseID;
     }
 
     public String getHouseID() {
         return houseID;
     }
 
-    public void setHouseID(String houseID) {
-        this.houseID = houseID;
+
+    public String getSellerID() {
+        return sellerID;
+    }
+
+    public void setSellerID(String sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public double getLocationX() {
         return locationX;
     }
 
+    public void setLocationX(double locationX) {
+        this.locationX = locationX;
+    }
+
     public double getLocationY() {
         return locationY;
     }
+
+    public void setLocationY(double locationY) {
+        this.locationY = locationY;
+    }
+
 }
