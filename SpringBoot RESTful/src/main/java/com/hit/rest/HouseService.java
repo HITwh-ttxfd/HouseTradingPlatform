0943b603aa5ea1eb05410e6d56edd46b642c888c
Neo1fight1 <17861080078@163.com>
2020-04-23 17:38:36 +0800

    Update HouseService

--- SpringBoot RESTful/src/main/java/com/hit/rest/HouseService.java
+++ SpringBoot RESTful/src/main/java/com/hit/rest/HouseService.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 @RestController
-public class HouseServer {
+public class HouseService {
     //测试用
     @RequestMapping(value="/test")
     public String test(){
