980c040e92641f4a8461933eaf61ba3e764148ba
Neo1fight1 <17861080078@163.com>
2020-04-24 21:16:21 +0800

    Update

--- SpringBoot RESTful/src/main/java/com/hit/rest/HouseController.java
+++ SpringBoot RESTful/src/main/java/com/hit/rest/HouseController.java
@@ -8,6 +8,8 @@ import entity.House;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController//            @RestController的作用等同于@Controller + @ResponseBody
 @CrossOrigin //              解决跨域问题
@@ -21,6 +23,17 @@ public class HouseController {
         house.setScore(0);
 //这里调用api解析地址产生地理编码
         ReadUrlUtil.getLocationDetail(house);
+
+        //设置房屋年份
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy");
+        Date date = new Date(System.currentTimeMillis());
+        String tempnow= formatter.format(date);
+        int now=Integer.valueOf(tempnow.toString());
+        String temppast= formatter.format(house.getTime());
+        int past=Integer.valueOf(temppast.toString());
+        int age=now-past;
+        house.setHousingAge(String.valueOf(age));
+
         HouseDBconnection p = new HouseDBconnection();
         p.addHouse(house);
     }
