5d574ffb63da0b8aadce50f970483085d2112ca4
Pegessi <wjaosu@163.com>
2020-04-24 22:09:59 +0800

    Pegessi

---
+++ Apache CXF/src/main/java/Image/imgRecognition.java
@@ -0,0 +1,40 @@
+package Image;
+
+import aip.utils.HTPrecognition;
+import codeBase64.ImageStore;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "Image.imgRec", serviceName = "imgRecognition")
+public class imgRecognition implements imgRec{
+    ImageStore img = new ImageStore();
+    @Override
+        public String imgClassify(@WebParam(name = "base")String base64){
+        String result = HTPrecognition.easydlImageClassify(base64);
+        return result;
+    }
+
+    @Override
+    public String departRes(@WebParam(name = "json")String jsonRes){
+        String result = HTPrecognition.departBase64(jsonRes);
+        return result;
+    }
+
+    @Override
+    public String storeImg(@WebParam(name = "base")String base,@WebParam(name = "fileName")String fileName,@WebParam(name = "id")String houseID) {
+        String check = img.base64ToImage(base,fileName,houseID);
+        if(check.equals("success")){
+            System.out.println(fileName+" store successfully.");
+            return check;
+        }else{
+            return check;
+        }
+    }
+
+    @Override
+    public String selectImg(@WebParam(name = "fileName")String fileName,@WebParam(name = "id")String houseID){
+        String result = img.selectUrl(fileName,houseID);
+        return result;
+    }
+}
