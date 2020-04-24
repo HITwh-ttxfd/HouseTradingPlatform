af66ec660ae85cd5f9f18a0f25d1838d3dba30f9
Pegessi <wjaosu@163.com>
2020-04-24 11:04:11 +0800

    Pegessi

--- SpringBoot RESTful/src/main/java/com/hit/rest/SRservice.java
+++ SpringBoot RESTful/src/main/java/com/hit/rest/SRservice.java
@@ -101,7 +101,7 @@ public class SRservice {
     // request相关--测试成功
     @RequestMapping(value = "/sendRequest/{senderID}/{receiverID}/{houseID}/{phone}", method = RequestMethod.GET)
     public void sendRequeset(@PathVariable("senderID")String senderID, @PathVariable("receiverID")String receiverID,
-                             @PathVariable(value = "houseID")String houseID,@PathVariable("phone")String phone,
+                             @PathVariable("houseID")String houseID,@PathVariable("phone")String phone,
                              @RequestParam(value = "date")String date,@RequestParam(value = "time")String time){
         User u1 = db.selectUser(senderID);
         User u2 = db.selectUser(receiverID);
