a3d36811af3fb1cb46d3caa9b9ad3261689a39e2
Pegessi <wjaosu@163.com>
2020-05-01 23:40:16 +0800

    Pegessi

--- SpringBoot RESTful/src/main/java/connector/DBconnection.java
+++ SpringBoot RESTful/src/main/java/connector/DBconnection.java
@@ -547,7 +547,7 @@ public class DBconnection{
                     list.add(new Conversation(senderID,name,date,content,read));
                 }
                 else if(index.add(receiverID)){
-                    list.add(new Conversation(senderID,name,date,content,read));
+                    list.add(new Conversation(receiverID,name,date,content,read));
                 }
             }
             System.out.println("Return conversations successfully.");
