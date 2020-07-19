e072df01cdb05f09623b55e8698dc8159c6e7e60
王琪淞 <44025398+thomeal@users.noreply.github.com>
2020-07-19 22:58:44 +0800

    scrollbar styles

--- WEB/src/App.vue
+++ WEB/src/App.vue
@@ -3,39 +3,36 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    export default {
+        name: 'App'
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-  height: 100%;
-}
-::-webkit-scrollbar{
-    width: 5px;
-    height: 5px;
-}
-::-webkit-scrollbar-thumb{
-    border-radius: 1em;
-    background-color: rgba(50,50,50,.3);
-}
-::-webkit-scrollbar-track{
-    border-radius: 1em;
-    background-color: rgba(50,50,50,.1);
-}
-*{
-  padding: 0;
-  margin: 0;
-}
-html,body{
-  width: 100%;
-  height: 100%;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: left;
+        color: #2c3e50;
+        height: 100%;
+        scrollbar-width: none; /* Firefox */
+        -ms-overflow-style: none; /* IE 10+ */
+    }
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    html, body {
+        width: 100%;
+        height: 100%;
+    }
 
 </style>
